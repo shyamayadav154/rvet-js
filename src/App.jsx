947206c28124ec1a4537fc7cfe72759c2dacdfdb
shyam <shyamayadav154@gmail.com>
2023-04-17 22:54:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className='grid place-content-center h-screen'>
+      <h1 className='text-2xl font-medium'>React + Vite + +Vitest + ESLint + Tailwind template</h1>
+    </main>
+  );
+}
+
+export default App;
